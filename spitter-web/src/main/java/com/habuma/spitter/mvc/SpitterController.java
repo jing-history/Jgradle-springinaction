b668bba117c7e96136a14bc59630b8db0f97bf2c
jingzing <louiswang0898@gmail.com>
2016-10-26 18:58:58 +0800

    Gradle Spring Intellij Idea下热部署实现“敏捷”开发

--- spitter-web/src/main/java/com/habuma/spitter/mvc/SpitterController.java
+++ spitter-web/src/main/java/com/habuma/spitter/mvc/SpitterController.java
@@ -1,21 +1,20 @@
 package com.habuma.spitter.mvc;
 
-import com.habuma.spitter.domain.Spittle;
+import com.habuma.spitter.domain.Spitter;
 import com.habuma.spitter.service.SpitterService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.inject.Inject;
-import java.util.List;
 
 /**
  * Created by wangyunjing on 2016/10/26.
  */
 @Controller
-@RequestMapping("/spitters")
+@RequestMapping("/spitter")
 public class SpitterController {
     private final SpitterService spitterService;
 
@@ -24,13 +23,12 @@ public class SpitterController {
         this.spitterService = spitterService;
     }
 
-    @RequestMapping(value="/{username}/spittles",
-            method=RequestMethod.GET)
+    @RequestMapping(value="/spittles", method=RequestMethod.GET)
     public String listSpittlesForSpitter(
-            @PathVariable String username, Model model) {
-        model.addAttribute(spitterService.getSpitter(username));
-        List<Spittle> spittlesForSpitter = spitterService.getSpittlesForSpitter(username);
-        model.addAttribute(spittlesForSpitter);
+            @RequestParam("spitterName") String username, Model model) {
+        Spitter spitter = spitterService.getSpitter(username);
+        model.addAttribute(spitter);
+        model.addAttribute(spitterService.getSpittlesForSpitter(username));
         return "spittles/list";
     }
 
--- spitter-web/src/main/webapp/WEB-INF/views/spittles/list.jsp
+++ spitter-web/src/main/webapp/WEB-INF/views/spittles/list.jsp
@@ -1,6 +1,21 @@
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="s" uri="http://www.springframework.org/tags"%>
+<di>
+    <h2>Spittles for ${spitter.username}</h2>
 
-<h2>Spitters</h2>
-<c:forEach items="${spitters}" var="spitter">
-    <c:out value="${spitter.username}"/> - <c:out value="${spitter.fullName}"/>
-</c:forEach>
+    <table cellspacing="15">
+        <c:forEach items="${spittleList}" var="spittle">
+            <tr>
+                <td>
+                    <img src="<s:url value="/resources/images/spitter_avatar.png" />" width="48" height="48">
+                </td>
+                <td>
+                    <a href="<s:url value="/spitters/${spittle.spitter.username}"/>">${spittle.spitter.username}</a>
+                    <c:out value="${spittle.text}"/>
+                    <br/>
+                    <c:out value="${spittle.when}"/>
+                </td>
+            </tr>
+        </c:forEach>
+    </table>
+</di>
